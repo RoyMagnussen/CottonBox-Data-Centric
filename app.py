a5b813a0532771a9ff12b99e9f7da18468941c2d
Roy Magnussen <roysgamesandmore@gmail.com>
2020-12-26 01:33:46 +0000

    Created get_total_shopping_cart_items function for shopping cart link.

--- app.py
+++ app.py
@@ -13,6 +13,22 @@ mongo = PyMongo(app)
 
 
 # Calls the function below when the route(s) below is visited by the user.
+def get_total_shopping_cart_items() -> int:
+    """
+    Retrives the total quantity of items in the shopping cart.
+    
+    Iterates through each item in the shopping cart collection and increments the `total_items` variable by the item quantity amount.
+
+    Returns:
+    
+        int: The total quantity of items in the shopping cart collection.
+    """
+
+    total_items = 0
+    for item in list(mongo.db.shopping_cart.find()):
+        total_items += item["quantity"]
+
+    return total_items
 @app.route("/")
 def en_index():
     return "TEST"
