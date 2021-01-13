d8080477544fa1ae2d2ed34996ba08fc11b8b264
Roy Magnussen <roysgamesandmore@gmail.com>
2021-01-13 22:10:04 +0000

    Created form fo checkout page.

--- forms.py
+++ forms.py
@@ -22,3 +22,32 @@ class EnContactForm(FlaskForm):
                             DataRequired(), Length(min=2, max=2000)])
 
     submit = SubmitField()
+
+
+class CheckoutForm(FlaskForm):
+    title = SelectField("Title", choices=[
+                        "Mr.", "Mrs.", "Miss.", "Ms.", "Mx.", "Rev", "Dr."], validators=[DataRequired()])
+
+    first_name = StringField("First Name", validators=[
+                             DataRequired(), Length(min=2, max=20)])
+
+    last_name = StringField("Last Name", validators=[
+                            DataRequired(), Length(min=2, max=20)])
+
+    address_line_1 = StringField("Address Line 1", validators=[
+                                 DataRequired(), Length(min=1, max=50)])
+
+    address_line_2 = StringField("Address Line 2", validators=[
+                                 DataRequired(), Length(min=1, max=50)])
+
+    address_line_3 = StringField("Address Line 3", validators=[
+                                 DataRequired(), Length(min=1, max=50)])
+
+    address_line_4 = StringField("Address Line 4", validators=[
+                                 DataRequired(), Length(min=1, max=50)])
+
+    post_code = StringField(
+        "Post Code/Zip", validators=[DataRequired(), Length(min=2)])
+
+    country = StringField("Country", validators=[
+                          DataRequired(), Length(min=2)])
