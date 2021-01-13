4782a936ec1ddb31aeb1289ee0e00ad625a8d479
Roy Magnussen <roysgamesandmore@gmail.com>
2021-01-13 00:00:57 +0000

    Create function to create cookie.

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,39 @@
 $(document).ready(function () {
-    $("#calloutCloseBtn").on("click", function () {
-        document.cookie = "callout_closed=True; path=/";
-    });
+  $("#cookieAcceptBtn").on("click", function () {
+    document.cookie = "cottonbox_accept=True";
+    $("#cookieConsentBanner").addClass("d-none");
+  });
+
+  $("#calloutCloseBtn").on("click", function () {
+    document.cookie = "callout_closed=True; path=/";
+  });
 });
 
 window.setTimeout(function () {
-    $("#flashMessage").fadeTo(500, 0).slideUp(500, function () {
-        $(this).remove();
+  $("#flashMessage")
+    .fadeTo(500, 0)
+    .slideUp(500, function () {
+      $(this).remove();
     });
 }, 3000);
 
-
 function initMap() {
-    const coords = {
-        lat: 25.04763651885303,
-        lng: 121.5503884556362
-    }
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: coords,
-        zoom: 20
-    });
-    const marker = new google.maps.Marker({
-        position: coords,
-        map: map,
-    });
+  const coords = {
+    lat: 25.04763651885303,
+    lng: 121.5503884556362,
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: coords,
+    zoom: 20,
+  });
+  const marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+  });
 }
 
 (function () {
-    emailjs.init("user_OFYhUA5JRmSJ85n6gKi0l");
+  emailjs.init("user_OFYhUA5JRmSJ85n6gKi0l");
 
-    emailjs.sendForm("gmail", "default_template", "#emailForm");
-
-
+  emailjs.sendForm("gmail", "default_template", "#emailForm");
 })();
